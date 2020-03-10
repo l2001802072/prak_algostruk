1e3191fb84a47ec59d13e6ec2336fd74e0436c61
l2001802072 <55442776+l2001802072@users.noreply.github.com>
2020-03-10 23:22:00 +0700

    Add files via upload

---
+++ L200180207_Modul2_H/no1_modul2.py
@@ -0,0 +1,45 @@
+# nomer1
+
+class Pesan(object):
+    """Sebuah class bernama Pesan.
+       Untuk memahami konsep Class dan Object.
+    """
+    def __init__(self, sebuahString):
+        self.teks = sebuahString
+    def cetakIni(self):
+        print(self.teks)
+    def cetakPakaiHurufKapital(self):
+        print(str.upper(self.teks))
+    def cetakPakaiHurufKecil(self):
+        print(str.lower(self.teks))
+    def jumKar(self):
+        return len(self.teks)
+    def cetakJumlahKarakterku(self):
+        print('kalimatku mempunyai', len(self.teks), 'karakter.')
+    def perbarui(self, stringBaru):
+        self.teks = stringBaru
+
+#a
+    def apakahTerkandung(self, a):
+        if a in self.teks:
+            return True
+        else :
+            return False
+#b
+    def hitungKonsonan(self):
+        kon = 0
+        x = self.teks
+        alfaKon = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+        for i in x:
+            if i in alfaKon:
+                kon += 1
+        return kon
+#c
+    def hitungVokal(self):
+        voc = 0
+        x = self.teks
+        alfaVoc = "aiueoAIUEO"
+        for i in x:
+            if i in alfaVoc:
+                voc += 1
+        return voc
